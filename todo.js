886d476e4bf6901f21aa6cf2eaf4682aa5b1d880
K.P <70147929+hyphen04@users.noreply.github.com>
2022-11-14 13:53:50 +0530

    update

---
+++ todo.js
@@ -0,0 +1,45 @@
+const todoList = () => {
+  const all = [];
+
+  let today = new Date().toLocaleDateString("en-CA");
+  const add = (todoItem) => {
+    all.push(todoItem);
+  };
+  const markAsComplete = (index) => {
+    all[index].completed = true;
+  };
+
+  const overdue = () => {
+    return all.filter((todo) => todo.dueDate < today);
+  };
+
+  const dueToday = () => {
+    return all.filter((todo) => todo.dueDate === today);
+  };
+
+  const dueLater = () => {
+    return all.filter((todo) => todo.dueDate > today);
+  };
+
+  const toDisplayableList = (list) => {
+    return list
+      .map(
+        (todo) =>
+          `${todo.completed ? `[x]` : `[ ]`} ${todo.title} ${
+            todo.dueDate != today ? todo.dueDate : " "
+          }`
+      )
+      .join("\n");
+  };
+
+  return {
+    all,
+    add,
+    markAsComplete,
+    overdue,
+    dueToday,
+    dueLater,
+  };
+};
+
+module.exports = todoList;
